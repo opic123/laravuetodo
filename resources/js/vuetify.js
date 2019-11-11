503d5dd36516a8e9bbfa3bbd22cf967b79310e46
opic <floresopic@gmail.com>
2019-11-11 13:56:35 +0800

    preset none

--- resources/js/vuetify.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify'; // Important! Not vuetify/lib, but just vuetify!!! 
-import 'vuetify/dist/vuetify.min.css';
-
-Vue.use(Vuetify);
-
-const opts = {};
-
-export default new Vuetify(opts);
